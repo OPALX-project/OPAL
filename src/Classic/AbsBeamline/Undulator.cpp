5012ff76c69e9300afb30dadc0219d69e48e8f6e
Arnau Albà <arnau.albajacas@psi.ch>
2020-08-21 14:13:42 +0200

    add warning if using Undulator without having compiled with Mithra

--- src/Classic/AbsBeamline/Undulator.cpp
+++ src/Classic/AbsBeamline/Undulator.cpp
@@ -89,6 +89,11 @@ void Undulator::accept(BeamlineVisitor &visitor) const {
 
 void Undulator::initialise(PartBunchBase<double, 3> *bunch, double &/*startField*/, double &/*endField*/) {
     RefPartBunch_m = bunch;
+    #ifndef OPAL_FEL
+    Inform msg("Undulator ", *gmsg);
+    msg << "Warning! You have defined an Undulator element but it will be" << endl;
+    msg << "interpreted as a Drift since OPAL was compiled without the OPAL_FEL option." << endl;
+    #endif
 }
 
 
