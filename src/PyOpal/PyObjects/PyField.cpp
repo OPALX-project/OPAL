99363ea5c9bbdd8c2033418710cd2f5ca7c1f026
Neveu <nneveu@stanford.edu>
2024-02-13 09:09:16 -0800

    working on adding T option to get field python function

--- src/PyOpal/PyObjects/PyField.cpp
+++ src/PyOpal/PyObjects/PyField.cpp
@@ -106,15 +106,22 @@ py::object get_field_value_t(OrbitThreader orbthreader,
 
 py::object get_field_value(double x, double y, double z, double t) {
     std::shared_ptr<Tracker> tracker = TrackRun::getTracker();
+
     ParallelCyclotronTracker* trackerCycl = 
-                        dynamic_cast<ParallelCyclotronTracker*>(tracker.get());
+        dynamic_cast<ParallelCyclotronTracker*>(tracker.get());
+    ParallelTTracker* trackerT = 
+        dynamic_cast<ParallelTTracker*>(tracker.get()); 
     if (trackerCycl != nullptr) {
         return get_field_value_cyclotron(x, y, z, t, trackerCycl);
     }
+    else (trackerT != nullptr){
+        return get_field_value_t(orbthreader, trackerT)
+    }
+    
     throw(OpalException("PyField::get_field_value",
-                        "Could not find a ParallelCyclotronTracker - get_field_value only works in OPAL-CYCL mode"));
+                        "Could not find a ParallelCyclotronTracker or ParallelTTracker."));
+    
 }
-
 
 
 // returns a *borrowed* pointer
